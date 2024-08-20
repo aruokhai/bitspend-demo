155a8e2ba46243c84206efc3f00c1c0cf1806fe9
Joshua Aruokhai <joshuaaruokhaitech@gmail.com>
2024-08-20 11:30:44 +0100

    fix security issue

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,14 +1,7 @@
+import { env } from "process";
+
 export const environment = {
   production: false,
-  firebase: {
-      apiKey: "AIzaSyASD-tAaBjnWxWu5Y9LOSfRKKf8dycKyG4",
-      authDomain: "bitspend-24b93.firebaseapp.com",
-      projectId: "bitspend-24b93",
-      storageBucket: "bitspend-24b93.appspot.com",
-      messagingSenderId: "101643559856",
-      appId: "1:101643559856:web:1202144faeea71363b6c75",
-      measurementId: "G-SEF2L55GJL"
-  },
   base_url: "http://127.0.0.1:5001/bitspend/europe-north1/development"
 
 };
